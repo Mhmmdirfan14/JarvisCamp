1459720c546ac6e7b4cb783c9d2377e416381717
Muhammad Irfan Hakim <muhammadirfanhakim45@gmail.com>
2024-05-24 11:21:38 +0700

    Pertemuan-6

---
+++ resources/views/perkenalan.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perkenalan</title>\
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body >
+    <div class="main-container">
+    <section id="hero">
+        <div class="hero-left">
+          <h3 class="pre-title">My name is</h3>
+          <h1 class="hero-name">Muhammad <span>Irfan Hakim</span></h1>
+          <p>
+            Let me introduce myself, my name is Irfan, I am now 22 years old. I am a student at the Nurul Fikri campus, my hobbies are playing games, learning coding and I like doing anything as long as it is interesting
+          </p>
+        </div>
+        <div class="hero-right">
+          <img src="assets/images/aku.jpg" alt="Poto Irfan" />
+        </div>
+      </section>
+    </div>
+</body>
+</html>
